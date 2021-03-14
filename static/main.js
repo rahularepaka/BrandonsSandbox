0a2efc8ea60124a4836f94762054adcd5d9e65ed
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-03-14 01:09:50 -0600

    first commit

---
+++ static/main.js
@@ -0,0 +1,36 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    document.getElementsByClassName("header-container")[0].style.height = "3rem";
+  } else {
+    document.getElementsByClassName("header-container")[0].style.height = "6rem";
+  }
+}
+
+window.onload=function() {
+  document.getElementById("content-container").style.opacity = 1;
+};
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {        
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
