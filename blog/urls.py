f00cdfd5eb99cbb340f50af467bd1e4c41e45c7a
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-05-23 11:00:36 -0500

    added custom 404 page

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('/allposts/', views.allposts, name='allposts'),
+    path('allposts/', views.allposts, name='allposts'),
     path('', views.index, name='index'),
-    path('/<page>', views.index, name='index'),
-    path('/post/<postid>', views.post, name='post'),
+    path('<page>', views.index, name='index'),
+    path('post/<postid>', views.post, name='post'),
 ]
