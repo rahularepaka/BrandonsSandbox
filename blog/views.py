97df5d019efbd70b24e7789521a745d188abb2ee
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-05-31 21:47:13 -0500

    added searchbar for blog posts

--- blog/views.py
+++ blog/views.py
@@ -4,20 +4,26 @@ from django.core import serializers
 from django.http import HttpResponse
 from django.http.response import JsonResponse
 import json
-
+from django.db.models import Q
 
 import math
 
 # Create your views here.
 def index(request, page=1):
-    page = int(page)
-    posts = Post.objects.all().order_by('id').reverse()
-    maxpage = math.ceil(len(posts)/7)
-    posts = posts[7*(page-1):7*page]
-    if page < maxpage:
-        return render(request, "index.html", {"posts":posts, "previous":page+1})
+    if request.method == 'POST':
+        query = request.POST['searchbar']
+        print(query)
+        posts = Post.objects.filter(Q(title__contains=query) | Q(content__contains=query)).order_by('id').reverse()
+        return render(request, "index.html", {"posts":posts, "previous":0, "searchquery":query})
     else:
-        return render(request, "index.html", {"posts":posts, "previous":0})
+        page = int(page)
+        posts = Post.objects.all().order_by('id').reverse()
+        maxpage = math.ceil(len(posts)/7)
+        posts = posts[7*(page-1):7*page]
+        if page < maxpage:
+            return render(request, "index.html", {"posts":posts, "previous":page+1})
+        else:
+            return render(request, "index.html", {"posts":posts, "previous":0})
 
 def post(request, postid):
     post = Post.objects.get(id=postid)
