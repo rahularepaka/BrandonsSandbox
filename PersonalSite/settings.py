ca54217e0ba853f8d86b05d882944471866b9bb4
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-08-03 17:41:57 -0500

    moved secret key to gitignore file

--- PersonalSite/settings.py
+++ PersonalSite/settings.py
@@ -17,18 +17,15 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ys9)+&=!uokl7+)wxd*50$3fj)at-vp6l)m9kuv2m3r^_1n(ny'
-
+from .secret_key import SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
